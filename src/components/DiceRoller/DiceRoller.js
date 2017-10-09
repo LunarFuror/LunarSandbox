f0fdcfc1e6ce712dc240df321c503570e4dad460
Grayson Lorenz <grayson@prominenttechnology.com>
2017-10-09 14:39:52 -0500

    adding more dice, and standard functionality

--- src/components/DiceRoller/DiceRoller.js
+++ src/components/DiceRoller/DiceRoller.js
@@ -18,15 +18,218 @@ export default {
     modStandardD12: 0,
     modStandardD20: 0,
     modStandardD100: 0,
-    numEotEDY: 0,
-    numEotEDG: 0,
-    numEotEDB: 0,
-    numEotEDR: 0,
-    numEotEDP: 0,
-    numEotEDB: 0,
-    diceType: 'standard'
+    numSkillYel: 0,
+    numSkillGre: 0,
+    numSkillBlu: 0,
+    numSkillRed: 0,
+    numSkillPur: 0,
+    numSkillBla: 0,
+    diceType: 'standard',
+    standardRollResults: [],
+    skillRollResults:[]
   }),
   methods:{
-    
+    clearResults: function () {
+      this.standardRollResults = [];
+    },
+    rollStandard: function () {
+      var fullRollResult = {d4Result: '', d6Result: '', d8Result: '', d10Result: '', d12Result: '', d20Result: '', d100Result: ''};
+
+      if(this.numStandardD4 > 0){
+        var totalD4rolls = 0;
+        var d4Roll = 0;
+
+        var d4Output = 'D4: ('
+        for(var d4i = 0; d4i < this.numStandardD4; d4i ++){
+          d4Roll = Math.floor(Math.random() * 4) +1
+          totalD4rolls += d4Roll;
+          if(d4i == this.numStandardD4-1){
+            d4Output += d4Roll;
+          }
+          else{
+            d4Output += d4Roll+', ';
+          }
+        }
+        var totalD4result = totalD4rolls + parseInt(this.modStandardD4);
+        d4Output += ') + ' + this.modStandardD4 + ' = ' + totalD4result + '\r\n';
+        fullRollResult.d4Result = d4Output;
+      }
+      
+      if(this.numStandardD6 > 0){
+        var totalD6rolls = 0;
+        var d6Roll = 0;
+
+        var d6Output = 'D6: ('
+        for(var d6i = 0; d6i < this.numStandardD6; d6i ++){
+          d6Roll = Math.floor(Math.random() * 6) +1
+          totalD6rolls += d6Roll;
+          if(d6i == this.numStandardD6-1){
+            d6Output += d6Roll;
+          }
+          else{
+            d6Output += d6Roll+', ';
+          }
+        }
+        var totalD6result = totalD6rolls + parseInt(this.modStandardD6);
+        d6Output += ') + ' + this.modStandardD6 + ' = ' + totalD6result + '\r\n';
+        fullRollResult.d6Result = d6Output;
+      }
+      
+      if(this.numStandardD8 > 0){
+        var totalD8rolls = 0;
+        var d8Roll = 0;
+
+        var d8Output = 'D8: ('
+        for(var d8i = 0; d8i < this.numStandardD8; d8i ++){
+          d8Roll = Math.floor(Math.random() * 8) +1
+          totalD8rolls += d8Roll;
+          if(d8i == this.numStandardD8-1){
+            d8Output += d8Roll;
+          }
+          else{
+            d8Output += d8Roll+', ';
+          }
+        }
+        var totalD8result = totalD8rolls + parseInt(this.modStandardD8);
+        d8Output += ') + ' + this.modStandardD8 + ' = ' + totalD8result + '\r\n';
+        fullRollResult.d8Result = d8Output;
+      }
+      
+      if(this.numStandardD10 > 0){
+        var totalD10rolls = 0;
+        var d10Roll = 0;
+
+        var d10Output = 'D10: ('
+        for(var d10i = 0; d10i < this.numStandardD10; d10i ++){
+          d10Roll = Math.floor(Math.random() * 10) +1
+          totalD10rolls += d10Roll;
+          if(d10i == this.numStandardD10-1){
+            d10Output += d10Roll;
+          }
+          else{
+            d10Output += d10Roll+', ';
+          }
+        }
+        var totalD10result = totalD10rolls + parseInt(this.modStandardD10);
+        d10Output += ') + ' + this.modStandardD10 + ' = ' + totalD10result + '\r\n';
+        fullRollResult.d10Result = d10Output;
+      }
+      
+      if(this.numStandardD12 > 0){
+        var totalD12rolls = 0;
+        var d12Roll = 0;
+
+        var d12Output = 'D12: ('
+        for(var d12i = 0; d12i < this.numStandardD12; d12i ++){
+          d12Roll = Math.floor(Math.random() * 12) +1
+          totalD12rolls += d12Roll;
+          if(d12i == this.numStandardD12-1){
+            d12Output += d12Roll;
+          }
+          else{
+            d12Output += d12Roll+', ';
+          }
+        }
+        var totalD12result = totalD12rolls + parseInt(this.modStandardD12);
+        d12Output += ') + ' + this.modStandardD12 + ' = ' + totalD12result + '\r\n';
+        fullRollResult.d12Result = d12Output;
+      }
+      
+      if(this.numStandardD20 > 0){
+        var totalD20rolls = 0;
+        var d20Roll = 0;
+
+        var d20Output = 'D20: ('
+        for(var d20i = 0; d20i < this.numStandardD20; d20i ++){
+          d20Roll = Math.floor(Math.random() * 20) +1
+          totalD20rolls += d20Roll;
+          if(d20i == this.numStandardD20-1){
+            d20Output += d20Roll;
+          }
+          else{
+            d20Output += d20Roll+', ';
+          }
+        }
+        var totalD20result = totalD20rolls + parseInt(this.modStandardD20);
+        d20Output += ') + ' + this.modStandardD20 + ' = ' + totalD20result + '\r\n';
+        fullRollResult.d20Result = d20Output;
+      }
+
+      if(this.numStandardD100 > 0){
+        var totalD100rolls = 0;
+        var d100Roll = 0;
+
+        var d100Output = 'D100: ('
+        for(var d100i = 0; d100i < this.numStandardD100; d100i ++){
+          d100Roll = Math.floor(Math.random() * 100) +1
+          totalD100rolls += d100Roll;
+          if(d100i == this.numStandardD100-1){
+            d100Output += d100Roll;
+          }
+          else{
+            d100Output += d100Roll+', ';
+          }
+        }
+        var totalD100result = totalD100rolls + parseInt(this.modStandardD100);
+        d100Output += ') + ' + this.modStandardD100 + ' = ' + totalD100result + '\r\n';
+        fullRollResult.d100Result = d100Output;
+      }
+      this.standardRollResults.push(fullRollResult);
+    },
+    rollSkill: function () {
+      var fullRollResult = {yelResult: '', greResult: '', bluResult: '', redResult: '', purResult: '', blaResult: '', };
+      var totalSuccess = 0;
+      var totalAdvantage = 0;
+      var totalTriumph = 0;
+      var totalFailure = 0;
+      var totalDisadvantage = 0;
+      var totalDispair = 0;
+      if(this.numSkillYel > 0){
+        var totalYelSuccess = 0;
+        var totalYelAdvantage = 0;
+        var totalYelTriumph = 0;
+        var yelOutput = 'Yellow: (';
+        for(var yeli = 0; yeli > this.numSkillYel; yeli ++){
+          yelRoll = Math.floor(Math.random() * 12) +1;
+          switch(yelRoll){
+            case 1: yelOutput += 'blank, '
+              break;
+            case 2:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 3:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 4:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 5:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 6:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 7:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 8:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 9:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 10:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 11:
+            case 1: yelOutput += 'blank, '
+              break;
+            case 12:
+            case 1: yelOutput += 'blank, '
+              break;
+          }
+        }
+      }
+    }
   }
 }
--- src/components/DiceRoller/DiceRoller.vue
+++ src/components/DiceRoller/DiceRoller.vue
@@ -5,7 +5,7 @@
         <label for="selDiceType">Dice Type</label>
         <select id="selDiceType" v-model="diceType">
           <option value="standard">Standard</option>
-          <option value="eote">EotE</option>
+          <option value="skill">Skill Dice</option>
         </select>
 
         <div v-if="diceType == 'standard'">
@@ -14,85 +14,100 @@
           </div>
           <div style="display: inline-block;">
             <div style="display: table;">
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D4:&nbsp;</span>
+                  <input type="number" v-model="numStandardD4" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D4&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD4" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D6:&nbsp;</span>
+                  <input type="number" v-model="numStandardD6" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D6&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD6" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D8:&nbsp;</span>
+                  <input type="number" v-model="numStandardD8" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D8&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD8" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D10:&nbsp;</span>
+                  <input type="number" v-model="numStandardD10" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D10&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD10" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D12:&nbsp;</span>
+                  <input type="number" v-model="numStandardD12" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D12&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD12" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D20:&nbsp;</span>
+                  <input type="number" v-model="numStandardD20" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D20&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD20" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
               <div style="display: table-row;">
                 <div style="display: table-cell;">
-                  <span class="lf-dice-label">D100:&nbsp;</span>
+                  <input type="number" v-model="numStandardD100" style="width:5em" min="0" max="999">
                 </div>
-                <div style="display: flex;">
-                  <input type="number" style="width:5em" min="0" max="999">
-                  <span class="lf-dice-label">&nbsp;+&nbsp;</span>
-                  <input type="number" style="width:5em" min="-999" max="999" >
+                <div style="display: table-cell; padding-top: 12px;">
+                  <span class="lf-dice-label">&nbsp;D100&nbsp;+&nbsp;</span>
+                </div>
+                <div style="display: table-cell;">
+                  <input type="number" v-model="modStandardD100" style="width:5em" min="-999" max="999" >
                 </div>
               </div>
+
             </div>
           </div>
           <div>
             <br/><hr class="lf-hr"/><br/>
-            <button class="lf-btn">Roll</button>
+            <button class="lf-btn" @click="rollStandard()">Roll</button>
           </div>
         </div>
 
-        <div v-if="diceType == 'eote'">
+        <div v-if="diceType == 'skill'">
           <div style="width:100%;">
             <br/><hr class="lf-hr"/><br/>
           </div>
@@ -159,8 +174,39 @@
 
 
     <div class="usa-width-one-half">
-      <div class="lf-standard-div">
-        Results will appear here
+      <div class="lf-standard-div" v-if="standardRollResults && standardRollResults.length">
+        <button class="lf-btn" @click="clearResults()">Clear Results</button>
+        <br/><br/><hr class="lf-hr"/>
+        <ul>
+          <li v-for="result of standardRollResults">
+            <span v-if="result.d4Result"> {{result.d4Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d6Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d8Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d10Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d12Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d20Result}}<br/></span>
+            <span v-if="result.d6Result"> {{result.d100Result}}<br/></span>
+            <br/>
+          </li>
+        </ul>
+      </div>
+      <div class="lf-standard-div" v-else-if="skillRollResults && skillRollResults.length">
+        <button class="lf-btn" @click="clearResults()">Clear Results</button>
+        <br/><br/><hr class="lf-hr"/>
+        <ul>
+          <li v-for="result of skillRollResults">
+            <span v-if="result.yelResult"> {{result.yelResult}}<br/></span>
+            <span v-if="result.greResult"> {{result.greResult}}<br/></span>
+            <span v-if="result.bluResult"> {{result.bluResult}}<br/></span>
+            <span v-if="result.redResult"> {{result.redResult}}<br/></span>
+            <span v-if="result.purResult"> {{result.purResult}}<br/></span>
+            <span v-if="result.blaResult"> {{result.blaResult}}<br/></span>
+            <br/>
+          </li>
+        </ul>
+      </div>
+      <div class="lf-standard-div" v-else>
+        Results will appear here.
       </div>
     </div>
   </div>
